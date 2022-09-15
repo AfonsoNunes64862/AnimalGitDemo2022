3f004ff42621ef7cdfc257b0a2494874170d8da8
Afonso <ane.nunes@campus.fct.unl.pt>
2022-09-15 22:14:48 +0100

    Initial version of our main class.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Afonso Nunes, n. 64682
+ * Demo for the Git lab on Software Engineering
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Animal git demo");
+    }
+}
