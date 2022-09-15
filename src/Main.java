a2ccdbe81e497fe26d056ccfc72a14356f57bad1
Afonso <ane.nunes@campus.fct.unl.pt>
2022-09-15 22:50:42 +0100

    More detail added to main!

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-/**
- * @author Afonso Nunes, n. 64682
- * Demo for the Git lab on Software Engineering
- */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Super animal git demo.");
+        System.out.println("Let's hear it from the pets");
+        Animal dog = new Dog("Bobby");
+        Animal cat = new Cat("Kitty");
+        System.out.println(dog.getName() + " says " + dog.speak());
+        System.out.println(cat.getName() + " says " + cat.speak());
     }
 }
